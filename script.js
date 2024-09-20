01d3122a5d37e52ee6fb366b723c34629f00a36b
LIan2nd <alfiannurusyaid19@gmail.com>
2024-09-21 00:15:11 +0700

    initialize, home n about done

---
+++ script.js
@@ -0,0 +1,22 @@
+// Navbar Active Script
+window.addEventListener('scroll', () => {
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('.nav-link');
+
+  let currentSection = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${currentSection}`) {
+      link.classList.add('active');
+    }
+  });
+});
