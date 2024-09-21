4ed09232c172911540ce4e00b37485a0a9d2e5c5
LIan2nd <alfiannurusyaid19@gmail.com>
2024-09-21 21:50:17 +0700

    complete portfolio

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+// Script for Form to GoogleSheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzWscwvmfPHsrD0PFYmytr3Hr3UHTo46rhpMQ6vB_-aPoSeKnm37EYNEP2OAshRNfM-Dw/exec'
+const form = document.forms['contact-form']
+const btnSend = document.querySelector('.btn-send');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  btnSend.classList.toggle('d-none');
+  btnLoading.classList.toggle('d-none');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      btnSend.classList.toggle('d-none');
+      btnLoading.classList.toggle('d-none');
+      myAlert.classList.toggle('d-none');
+
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch(error => console.error('Error!', error.message))
+});
